74e23ed1d947e5e417e5570d821bc271acf34195
Gamya <Gamya@LAPTOP-ERD397M4>
2024-11-18 09:09:26 +0530

    sync fork class added

---
+++ simpleMaven/src/test/java/syncFork/ToLearnSyncFork.java
@@ -0,0 +1,13 @@
+package syncFork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnSyncFork {
+	@Test
+	public void sync() {
+		Reporter.log("sync fork class added");
+		
+	}
+
+}
